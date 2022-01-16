4e114624b0e56f3102b31a7b21ad18e36ce0caea
Roberto Bendinelli <robertobendi@outlook.it>
2022-01-16 20:12:35 +0100

    NEW: buy/sell buttons

--- src/Crypto.js
+++ src/Crypto.js
@@ -54,8 +54,22 @@ class Crypto extends React.Component {
   };
 
   handleAction = (event) => {
+    const textarea = document.getElementById("log");
     event.preventDefault();
-    //console.log(event.target[0].value);
+    textarea.value +=
+      "invested " +
+      document.getElementById("moneytospend").value +
+      " in " +
+      document.getElementById("symbol").value +
+      " at " +
+      this.state.stockChartYValues[99] +
+      "\n";
+    const output1 = document.getElementById("amountinvested");
+    output1.value = document.getElementById("moneytospend").value;
+
+    const output2 = document.getElementById("investedat");
+    output2.value = this.state.stockChartYValues[99];
+      
     this.fetchStock(event.target.key.value, event.target.symbol.value);
   };
 
@@ -87,6 +101,7 @@ class Crypto extends React.Component {
                 <label class="col-form-label">
                   Crypto symbol <br></br>
                   <input
+                    id="symbol"
                     type="text"
                     name="symbol"
                     className={classes.inputsymbol}
@@ -130,42 +145,120 @@ class Crypto extends React.Component {
         </div>
 
         <div class="col-6">
-          <h1>Actions</h1>
+          <div>
+            <h1>1.Take some cash</h1>
+            <form onSubmit={this.handleAction}>
+              <div class="row">
+                <div class="col-5">
+                  <label class="col-form-label">
+                    Amount to invest <br></br>
+                    <input
+                      type="text"
+                      id="moneytospend"
+                      name="moneytospend"
+                      className={classes.input}
+                      ref={(node) => (this.inputNode = node)}
+                    />
+                    <br></br>
+                    <span id="textHelpBlock" class="form-text text-muted">
+                      only numbers, the currency will be USD
+                    </span>
+                  </label>
+                </div>
 
-          <form onSubmit={this.handleAction}>
-            <div class="row">
-              <div class="col-5">
-                <label class="col-form-label">
-                  Amount to invest <br></br>
-                  <input
-                    type="text"
-                    name="money"
-                    className={classes.input}
-                    ref={(node) => (this.inputNode = node)}
-                  />
-                  <br></br>
-                  <span id="textHelpBlock" class="form-text text-muted">
-                    only numbers, the currency will be USD
-                  </span>
-                </label>
+                <div class="col-3">
+                  <button type="submit" className={classes.buttonbuy}>
+                    Buy
+                  </button>
+                </div>
+                <div class="col-3">
+                  <button type="submit" className={classes.buttonsell}>
+                    Sell
+                  </button>
+                </div>
               </div>
+            </form>
+          </div>
 
-              <div class="col-3">
-                <button type="submit" className={classes.buttonbuy}>
-                  Buy
-                </button>
+          <div>
+            <h3>2. "Invest it"</h3>
+            <form onSubmit={this.handleAction}>
+              <div class="row">
+                <div class="col-2">
+                  <label class="col-form-label">
+                    Amount <br></br>
+                    <input
+                      type="text"
+                      id="amountinvested"
+                      name="amountinvested"
+                      className={classes.inputsymbol}
+                      ref={(node) => (this.inputNode = node)}
+                    />
+                  </label>
+                </div>
+
+                <div class="col-3">
+                  <label class="col-form-label">
+                    "invested" at <br></br>
+                    <input
+                      type="text"
+                      id="investedat"
+                      name="investedat"
+                      className={classes.display}
+                      ref={(node) => (this.inputNode = node)}
+                    />
+                  </label>
+                </div>
+
+
               </div>
-              <div class="col-3">
-                <button type="submit" className={classes.buttonsell}>
-                  Sell
-                </button>
+            </form>
+          </div>
+
+          <div>
+            <h3>3. Watch it burn</h3>
+            <form onSubmit={this.handleAction}>
+              <div class="row">
+                <div class="col-2">
+                  <label class="col-form-label">
+                    Amount <br></br>
+                    <input
+                      type="text"
+                      id="amountinvested"
+                      name="amountinvested"
+                      className={classes.inputsymbol}
+                      ref={(node) => (this.inputNode = node)}
+                    />
+                  </label>
+                </div>
+
+                <div class="col-3">
+                  <label class="col-form-label">
+                    "invested" at <br></br>
+                    <input
+                      type="text"
+                      id="investedat"
+                      name="investedat"
+                      className={classes.display}
+                      ref={(node) => (this.inputNode = node)}
+                    />
+                  </label>
+                </div>
+
+
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
 
           <div>
             <label for="log">Actions Log</label>
-            <textarea className={classes.textarea} id="log" name="log" rows="10" cols="80"></textarea>
+            <textarea
+              className={classes.textarea}
+              id="log"
+              name="log"
+              rows="10"
+              cols="80"
+            ></textarea>
           </div>
         </div>
       </div>
